b6d302bc7591a85931e19facc115a94e022e9c54
Viktor <viktor.avgn@yandex.ru>
2025-11-19 23:36:27 +0700

    инкапсуляция js файлов; прикручена отправка данных формы-модалки на тестовый сервер

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,25 +1,29 @@
-const navbar = document.querySelector('.header__nav')
-const links = navbar.querySelectorAll('a')
+const smoothScroll = () => {
+  const navbar = document.querySelector('.header__nav')
+  const links = navbar.querySelectorAll('a')
 
-links.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault()
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault()
 
-    const section = document.querySelector(link.getAttribute('href'))
+      const section = document.querySelector(link.getAttribute('href'))
 
-    if (section) {
-      // Стандартная реализация плавного скролла
-      // section.scrollIntoView({
-      //   block: 'start',
-      //   behavior: 'smooth',
-      // })
+      if (section) {
+        // Стандартная реализация плавного скролла
+        // section.scrollIntoView({
+        //   block: 'start',
+        //   behavior: 'smooth',
+        // })
 
-      // Реализация кросс-браузерного плавного скролла с использованием библиотеки seamless-scroll-polyfill
-      seamless.scrollIntoView(section, {
-        behavior: 'smooth',
-        block: 'start',
-        inline: 'center',
-      })
-    }
+        // Реализация кросс-браузерного плавного скролла с использованием библиотеки seamless-scroll-polyfill
+        seamless.scrollIntoView(section, {
+          behavior: 'smooth',
+          block: 'start',
+          inline: 'center',
+        })
+      }
+    })
   })
-})
+}
+
+smoothScroll()
